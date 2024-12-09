16b2611f5f4e4c728b57501685b7307ffa1a7e46
JACKRCG <jackcarhgon2@gmail.com>
2024-12-08 23:46:12 -0500

    corrección de orden de migraciones y seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             SpecialtiesTableSeeder::class,
-            WorkDaysTableSeeder::class,
-            AppointmentsTableSeeder::class
+            AppointmentsTableSeeder::class,
+            WorkDaysTableSeeder::class            
         ]);
     }
 }
